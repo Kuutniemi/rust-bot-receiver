3d7e607fae857522220ba6abd6e18c04160d10a0
Kuutniemi <jkuutniemi@outlook.com>
2025-04-01 00:09:31 +0300

    change

--- app.js
+++ app.js
@@ -8,6 +8,31 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
+const counts = async () => {
+  try {
+    const helis = await prisma.heliMinusOne.findMany({});
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const todayHelis = await prisma.heliMinusOne.count({
+      where: {
+        when: {
+          gte: today, // Start of today
+          lt: new Date(today.getTime() + 86400000), // Start of tomorrow
+        },
+      },
+    });
+
+    res
+      .status(200)
+      .send(`Helis fucked today: ${todayHelis}. Total: ${helis.length}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to retrieve data");
+  }
+};
+
 // Sample route to create a user
 app.post("/heliminusone", async (req, res) => {
   console.log("Request for -1 received:", req.body);
@@ -18,7 +43,15 @@ app.post("/heliminusone", async (req, res) => {
         who: req.body.who || "Unknown",
       },
     });
-    res.status(201).json(newEntry);
+    // res.status(201).json(newEntry);
+    if (!newEntry) {
+      return res
+        .status(404)
+        .json({ error: "Failed to add -1 to the database" });
+    }
+    const returnMessage = counts();
+    console.log("Return message:", returnMessage);
+    res.status(201).json({ message: "Successfully added -1", newEntry });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to add -1 to the database" });
